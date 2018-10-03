303eaaca3b32fe39deafde158ae3ba21564d17ab
Kam1986 <kam@live.dk>
2018-10-03 14:34:04 +0200

    added changes

--- Uge 4/Aflevering/src/main.c
+++ Uge 4/Aflevering/src/main.c
@@ -11,22 +11,38 @@
 #include "compute.h"
 
 // major opcodes
-#define RETURN         0x0
-#define REG_ARITHMETIC 0x1
-#define REG_MOVQ       0x2
-#define REG_MOVQ_MEM   0x3
-#define CFLOW          0x4
-#define IMM_ARITHMETIC 0x5
-#define IMM_MOVQ       0x6
-#define IMM_MOVQ_MEM   0x7
-#define LEAQ2          0x8
-#define LEAQ3          0x9
-#define LEAQ6          0xA
-#define LEAQ7          0xB
-#define IMM_CBRANCH    0xF
+#define RETURN         0x0 // return              - size 2
+#define REG_ARITHMETIC 0x1 // reg       ->  reg   - size 2
+#define REG_MOVQ       0x2 // reg       ->  reg   - size 2
+#define REG_MOVQ_MEM   0x3 // reg       <-> mem   - size 2
+#define CFLOW          0x4 // ??
+#define IMM_ARITHMETIC 0x5 // imm       ->  reg   - size 3
+#define IMM_MOVQ       0x6 // imm       ->  reg   - size 3
+#define IMM_MOVQ_MEM   0x7 // imm mem   <-> reg   - size 3
+#define LEAQ2          0x8 // reg       ->  reg   - size 2
+#define LEAQ3          0x9 // reg       ->  reg   - size 3
+#define LEAQ6          0xA // reg       ->  reg   - size 3
+#define LEAQ7          0xB // reg       ->  reg   - size 4
+#define IMM_CBRANCH    0xF // 
 
-#define JMP 0xF
-#define CALL 0xE
+// minor opcodes memory
+#define MR      0x1 // reg      -> mem
+#define IMREG   0x4 // imm      -> reg
+#define IRM     0x5 // reg      -> imm mem
+#define RM      0x9 // mem      -> reg
+#define IMR     0xD // imm mem  -> reg
+
+// minor opcodes
+
+// minor opcodes
+#define JMP     0xF
+#define CALL    0xE
+
+
+
+#define SIZE2  from_int(2)
+#define SIZE3  from_int(3)
+#define SIZE4  from_int(4)
 
 int main(int argc, char* argv[]) {
     // Check command line parameters.
@@ -88,11 +104,55 @@ int main(int argc, char* argv[]) {
 
         // decode instruction type
         // read major operation code
-        bool is_return = is(RETURN, major_op);
-        // Add further decoding from here
+        /* 2 */ bool is_return          = is(RETURN, major_op);
+        
+        // Add further decoding from here - DONE
 
-        // determine instruction size - we only understand return, so fix it at 2
-        val ins_size = from_int(2);
+        /* 2 */ bool is_reg_arithmetic  = is(REG_ARITHMETIC, major_op);
+        /* 2 */ bool is_reg_movq        = is(REG_MOVQ, major_op);
+        /* 2 */ bool is_reg_movq_mem    = is(REG_MOVQ_MEM, major_op);
+        /* ? */ bool is_cflow           = is(CFLOW, major_op);
+        /* 3 */ bool is_imm_arithmetic  = is(IMM_ARITHMETIC, major_op);
+        /* 3 */ bool is_imm_movq        = is(IMM_MOVQ, major_op);
+        /* 3 */ bool is_imm_movq_mem    = is(IMM_MOVQ_MEM, major_op);
+        /* 2 */ bool is_leaq2           = is(LEAQ2, major_op);
+        /* 3 */ bool is_leaq3           = is(LEAQ3, major_op);
+        /* 3 */ bool is_leaq6           = is(LEAQ6, major_op);
+        /* 4 */ bool is_leaq7           = is(LEAQ7, major_op);
+             
+
+        bool is_imm = 
+            or(is_imm_arithmetic,
+            or(is_imm_movq,
+               is_imm_movq_mem,));
+
+        bool is_lea = 
+            or(is_leaq2,
+            or(is_leaq3,
+            or(is_leaq6,
+               is_leaq7)));
+
+        // set this to 0 if not size 3
+        val is_size3 =
+                use_if(
+                    or(is_imm_arithmetic,
+                    or(is_imm_movq,
+                    or(is_imm_movq_mem,
+                    or(is_leaq3,
+                       is_leaq6)))),
+                    SIZE3
+                );
+
+        // set this to 0 if not size 4
+        val is_size4 = use_if(is_leaq7,SIZE4);
+        
+        // determine instruction size - 2,3 or 4
+        val ins_size =
+            or(is_size3, 
+            or(is_size4, 
+               SIZE2));  // default value for size
+
+
 
         // control signals for memory access - you will want to change these
         bool is_load = false;
@@ -112,7 +172,10 @@ int main(int argc, char* argv[]) {
         // perform calculations - Return needs no calculation. you will want to change this.
         // Here you should hook up a call to compute_execute with all the proper
         // arguments in place
-        val compute_result = from_int(0); // you will want to change this.
+        val compute_result = compute_execute(
+                            // TODO need to give the right input.
+
+                            ).val; // you will want to change this.
 
         // succeeding instruction in memory
         val pc_inc  = add(pc, ins_size);
